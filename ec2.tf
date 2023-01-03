440708c49bd6ad53d471c6081eb33269f7f6f714
EC2 Default User <ec2-user@ip-172-31-93-207.ec2.internal>
2023-01-03 10:38:36 +0000

    just

---
+++ ec2.tf
@@ -0,0 +1,29 @@
+# Creating 1st EC2 instance in Public Subnet
+resource "aws_instance" "demoinstance" {
+ ami = "ami-0b5eea76982371e91"
+ instance_type = "t2.micro"
+ #count = 1
+ key_name = "ca"
+ vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
+ subnet_id = "${aws_subnet.public-subnet-1.id}"
+ associate_public_ip_address = true
+ user_data = "${file("data.sh")}"
+tags = {
+ Name = "My Public Instance"
+}
+}
+
+# Creating 2nd EC2 instance in Public Subnet
+resource "aws_instance" "demoinstance1" {
+ ami = "ami-0b5eea76982371e91"
+ instance_type = "t2.micro"
+ #count = 1
+ key_name = "ca"
+ vpc_security_group_ids = ["${aws_security_group.demosg.id}"]
+ subnet_id = "${aws_subnet.public-subnet-2.id}"
+ associate_public_ip_address = true
+ user_data = "${file("data.sh")}"
+tags = {
+ Name = "My Public Instance 2"
+}
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+# update provider
+#AWS as our provider
+
+provider "aws"{
+region="us-east-1"
+access_key="AKIA3LLJNDBVMHFMMS5P"
+secret_key="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"
+}
+
